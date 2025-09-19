457f966498f0f3e711acdf362e7aab91009820c9
Arthur Ronaldo da Silva <arthurronaldodasilvaa@gmail.com>
2025-09-19 16:24:14 -0300

    tipos-primitivos

---
+++ download-arquivos/dados-primitivos-e-o-erro-de-50-milhoes/JavaNinja/src/TipoDeDados/DadosPrimitivos.java
@@ -0,0 +1,20 @@
+package TipoDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        /*
+        * Dados Primitivos - int, double, float, char, boolean, short
+        * Objetivo da aula: Criar um ninja - Naruto -
+        * */
+
+        int idade = 16; // Valor máximo: 2 147 483 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        long saldoBancario = 999999;
+
+
+
+    }
+}
