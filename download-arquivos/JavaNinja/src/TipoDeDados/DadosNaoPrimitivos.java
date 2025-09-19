564b2bc8da792b6a26d8470170a6b26ab5f05db6
Arthur Ronaldo da Silva <arthurronaldodasilvaa@gmail.com>
2025-09-19 18:26:05 -0300

    nao-primitivos

--- download-arquivos/JavaNinja/src/TipoDeDados/DadosNaoPrimitivos.java
+++ download-arquivos/JavaNinja/src/TipoDeDados/DadosNaoPrimitivos.java
@@ -1,7 +1,20 @@
 package TipoDeDados;
 
+import java.util.Arrays;
+
 public class DadosNaoPrimitivos {
     public static void main(String[] args) {
+
+        /*
+        * Dados não primitivos: String, Array, Class, enum
+        * Objetivo: Criar um ninja, e atribuir métodos a ele
+        * */
+
+        String nome = "Naruto Uzumaki";
+        System.out.println(nome.toUpperCase());//CAPSLOCK
+
+        String aldeia = "Aldeia da Folha";
+        System.out.println(aldeia.toLowerCase());
 
     }
 }
