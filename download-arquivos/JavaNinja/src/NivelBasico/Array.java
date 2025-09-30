d287ae9d5fefd73ac9da4bc68e314dab3ee695e1
Arthur Ronaldo da Silva <arthurronaldodasilvaa@gmail.com>
2025-09-30 13:54:22 -0300

    Update Array.java

--- download-arquivos/JavaNinja/src/NivelBasico/Array.java
+++ download-arquivos/JavaNinja/src/NivelBasico/Array.java
@@ -3,13 +3,40 @@ package NivelBasico;
 public class Array {
     public static void main(String[] args) {
 
-        String[] ninja = new String[4];
+        String[] ninja = new String[5];
         ninja[0] = "Naruto Uzumaki";
         ninja[1] = "Sasuke Uchiha";
         ninja[2] = "Sakura Haruno";
         ninja[3] = "Hinata Hyuga";
+        ninja[4] = "Kakashi Hatake";
 
-        System.out.println(ninja[0]);
+        // Redeclarar
+        ninja = new String[7];
+        ninja[0] = "Hashirama Senju";
+        ninja[1] = "Tobirama Senju";
+        ninja[2] = "Hiruzen Sarutobi";
+        ninja[3] = "Minato Namikazi";
+        ninja[4] = "Tsunade";
+        ninja[5] = "Kakashi Hatake";
+        ninja[6] = "Naruto Uzumaki";
+        // System.out.println(ninja[2]);
+
+        // For para listar array
+
+        int h = 0;
+
+        for (int i = 0; i < ninja.length; i++) {
+            h++;
+            System.out.println("O " + h + "° Hokage é: " + ninja[i]);
+        }
+
+        System.out.println("---------------\n");
+
+        h = 0;
+        for (String s : ninja) {
+            h++;
+            System.out.println("O " + h + "° Hokage é: " + s);
+        }
 
     }
 }
