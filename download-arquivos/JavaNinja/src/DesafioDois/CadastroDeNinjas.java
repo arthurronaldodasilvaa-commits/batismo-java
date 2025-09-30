74b7f5fe7933fde7a9ef59064da89a803717664e
Arthur Ronaldo da Silva <arthurronaldodasilvaa@gmail.com>
2025-09-30 14:33:18 -0300

    correções e boas práticas.

--- download-arquivos/JavaNinja/src/DesafioDois/CadastroDeNinjas.java
+++ download-arquivos/JavaNinja/src/DesafioDois/CadastroDeNinjas.java
@@ -18,11 +18,10 @@ public class CadastroDeNinjas {
 
         /* Outras Variavéis */
         boolean s = false;
-        short h = 0;
         /**/
 
         /* BODY */
-        while (s == false){
+        while (!s){
             System.out.println("\n===== Menu Ninja =====");
             System.out.println("1. Cadastrar Ninja");
             System.out.println("2. Listar Ninjas");
@@ -31,23 +30,26 @@ public class CadastroDeNinjas {
             escolha = t.nextShort();
             switch (escolha) {
                 case 1:
+                    if (ninja >= 10) { // ← Verificar limite
+                        System.out.println("Limite de 10 ninjas atingido!");
+                        break;
+                    }
                     System.out.println("\nCadastrando ninja...");
                     System.out.print("Qual o nome do ninja: ");
                     t.nextLine();
                     nome = t.nextLine();
                     ninjas[ninja] = nome;
                     ninja++;
-                    System.out.println("\nNinja cadastrado.\n");
+                    System.out.println("\nNinja cadastrado.");
                     break;
+
                 case 2:
                     System.out.println("\nEstes são os ninjas registrados:");
-                    for (String a : ninjas) {
-                        h++;
-                        if (h == 1 && a == null) {
-                            System.out.println("Nenhum ninja cadastrado...");
-                        }
-                        if (a != null) {
-                            System.out.println("O " + h + "° ninja da lista é " + a);
+                    if (ninja == 0) {
+                        System.out.println("Nenhum ninja cadastrado...");
+                    } else {
+                        for (int i = 0; i < ninja; i++) {
+                            System.out.println("O(a) " + (i+1) + "° ninja da lista é " + ninjas[i]);
                         }
                     }
                     break;
